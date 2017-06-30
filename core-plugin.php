66c1b0c47490331549ef7ddbd67a7c650b8bd1d0
Phuc Do <phucdo@dagiac.com>
2017-06-30 16:01:05 +0700

    Main file

---
+++ core-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Core Plugin
+* Description: Core application of this website.
+* Version: 1.0
+* Author: Dagiac <phucdo@dagiac.com>
+* Author URI: https://www.dagiac.com/
+* License: PTB
+*/
